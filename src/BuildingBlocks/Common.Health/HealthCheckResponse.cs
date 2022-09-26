19510f448a7b4b88e5160ccb8caa4108f4e9eeb4
Salvation Kemuel <skemuel007@gmail.com>
2022-09-26 16:43:25 +0100

    Common file for custom reponse for checkresultoptions

---
+++ src/BuildingBlocks/Common.Health/HealthCheckResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Health
+{
+    public class HealthCheckResponse
+    {
+        public string status { get; set; }
+        public string key { get; set; }
+        public string description { get; set; }
+        public IEnumerable<string> tags { get; set; }
+        public string exception { get; set; }
+    }
+}
---
+++ src/BuildingBlocks/Common.Health/HealthCheckResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Health
+{
+
+    // The aggregation of all health check responses, even if one is unhealthy, 'Status' will be unhealthy.
+    public class HealthCheckResult
+    {
+        public string status { get; set; }
+        public IEnumerable<HealthCheckResponse> entries { get; set; }
+        public string totalDuration { get; set; }
+    }
+}
